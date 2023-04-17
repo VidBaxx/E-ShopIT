8b2fb28abd911a2146cfd22f69888e3e8f26302e
vidBaxx <kariukiedward06@gmail.com>
2023-04-17 13:30:48 +0300

    seller profile

--- src/components/App.js
+++ src/components/App.js
@@ -2,23 +2,22 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './App.css';
 import Home from './Home';
-<<<<<<< HEAD
+
 import SellerIn from './seller Acc/seller';
-=======
+
 import Login from './pages/auth/Login';
 import Profile from './profile/Profile';
 import Signin from './pages/auth/Signin';
 
->>>>>>> 0f1116776df81f703a6078aed62237be0d95e041
+
 
 
 function App() {
    return (
     
     <div className="App">
-<<<<<<< HEAD
-      <SellerIn/>
-=======
+
+      {/* <SellerIn/> */}
       <Router>
         <Routes>
         <Route path="/" element={<Home />} />
@@ -27,7 +26,7 @@ function App() {
         <Route path='/signin' element ={<Signin/>} />
       </Routes>
       </Router>
->>>>>>> 0f1116776df81f703a6078aed62237be0d95e041
+
       
     </div>
   );
